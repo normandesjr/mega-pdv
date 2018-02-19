b1c5c86218db82e6bb4c0d233b64900b104fdacc
Normandes Junior <normandesjr@gmail.com>
2018-02-19 09:11:36 -0300

    Refatorando metodo de importacao de produto

--- app/etl/importadorProduto.js
+++ app/etl/importadorProduto.js
@@ -2,10 +2,18 @@ let etl = require('etl')
 let produtoDAO = require('../dao/produtoDAO')
 
 function importar (planilhaAnexa) {
+  let planilhaEmDisco = salvarPlanilhaEmDisco(planilhaAnexa)
+  processarPlanilha(planilhaEmDisco)
+}
+
+function salvarPlanilhaEmDisco (planilhaAnexa) {
   let nomeArquivo = planilhaAnexa.submittedFileName
   let planilhaEmDisco = new java.io.File(nomeArquivo).getAbsolutePath() // eslint-disable-line
   planilhaAnexa.write(planilhaEmDisco)
+  return planilhaEmDisco
+}
 
+function processarPlanilha (planilhaEmDisco) {
   etl({})
     .set_input_xlsx_file(planilhaEmDisco, 0)
     .take_fields_from_header_row()
