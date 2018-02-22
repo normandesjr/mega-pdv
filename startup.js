dd17a6bbde852b6fad19936199684a36647e3c4d
Bruno Machado Brandão <brunin5010@gmail.com>
2018-02-22 15:06:04 -0300

    Removendo hashing do arquivo  principal

--- startup.js
+++ startup.js
@@ -7,7 +7,4 @@ router.addMiddleware(auth)
 const routes = require('./routes/index')
 routes.registrar(router)
 
-const ha = require('./app/util/hashingUtil')
-show(ha.gerarHash('admin'))
-
 server.createServer(8778, router)
